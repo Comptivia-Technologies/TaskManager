bfb9901504b5fd886755f42417c16bebab840541
Sreejith-Comptivia <sreejith.murali@comptivia.com>
2026-02-11 17:36:07 +0530

    backend-flow-resolved-3

--- backend/Workload.API/Program.cs
+++ backend/Workload.API/Program.cs
@@ -6,7 +6,7 @@ using Workload.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container for Workload.API
+// Add services to the container for Workload.API service
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
