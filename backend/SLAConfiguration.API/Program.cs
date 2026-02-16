0df8cb51b669d10d3442c806ca1f75e1a19ba914
Sreejith-Comptivia <sreejith.murali@comptivia.com>
2026-02-16 11:58:22 +0530

    db-changed

--- backend/SLAConfiguration.API/Program.cs
+++ backend/SLAConfiguration.API/Program.cs
@@ -28,7 +28,7 @@ if (string.IsNullOrEmpty(connectionString))
     var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD")
         ?? throw new InvalidOperationException("DB_PASSWORD environment variable is required");
     
-    connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
+    connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword};SSL Mode=Require";
 }
 
 builder.Services.AddDbContext<SLAConfigurationDbContext>(options =>
